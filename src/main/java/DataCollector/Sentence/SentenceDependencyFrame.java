cff8bb715d36109735491ebcd6e476bac95dfd25
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2020-03-25 10:20:34 +0300

    Show dependency unannotated files.

--- src/main/java/DataCollector/Sentence/SentenceDependencyFrame.java
+++ src/main/java/DataCollector/Sentence/SentenceDependencyFrame.java
@@ -1,7 +1,10 @@
 package DataCollector.Sentence;
 
 import AnnotatedSentence.AnnotatedCorpus;
+import AnnotatedSentence.AnnotatedSentence;
+import AnnotatedSentence.AnnotatedWord;
 import DataCollector.ParseTree.EditorPanel;
+import DependencyParser.UniversalDependencyRelation;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -19,6 +22,29 @@ public class SentenceDependencyFrame extends AnnotatorFrame {
         super("dependency");
         AnnotatedCorpus corpus;
         corpus = new AnnotatedCorpus(new File(EditorPanel.TURKISH_PHRASE_PATH));
+        JMenuItem itemShowUnannotated = addMenuItem(projectMenu, "Show Unannotated Files", KeyStroke.getKeyStroke(KeyEvent.VK_U, ActionEvent.CTRL_MASK));
+        itemShowUnannotated.addActionListener(e -> {
+            int count = 0;
+            String result = JOptionPane.showInputDialog(null, "How many sentences you want to see:", "",
+                    JOptionPane.PLAIN_MESSAGE);
+            int numberOfSentences = Integer.parseInt(result);
+            for (int i = 0; i < corpus.sentenceCount(); i++){
+                AnnotatedSentence sentence = (AnnotatedSentence) corpus.getSentence(i);
+                for (int j = 0; j < sentence.wordCount(); j++){
+                    AnnotatedWord word = (AnnotatedWord) sentence.getWord(j);
+                    UniversalDependencyRelation universalDependencyRelation = word.getUniversalDependency();
+                    if (word.getName() != null && universalDependencyRelation == null){
+                        AnnotatorPanel annotatorPanel = generatePanel(EditorPanel.TURKISH_PHRASE_PATH, sentence.getFileName());
+                        addPanelToFrame(annotatorPanel, sentence.getFileName());
+                        count++;
+                        if (count == numberOfSentences){
+                            return;
+                        }
+                        break;
+                    }
+                }
+            }
+        });
         JMenuItem itemViewAnnotated = addMenuItem(projectMenu, "View Annotations", KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
         itemViewAnnotated.addActionListener(e -> {
             new ViewDependencyAnnotationFrame(corpus);
