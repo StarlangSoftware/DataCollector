de4a21aceb24caee23b961c773b33de6b08e9939
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-09 14:47:48 +0300

    Removed SemanticRelationFrame.java.

--- src/main/java/DataCollector/WordNet/TestInterlingualRelationFrame.java
+++ src/main/java/DataCollector/WordNet/TestInterlingualRelationFrame.java
@@ -7,7 +7,7 @@ import java.util.Locale;
 public class TestInterlingualRelationFrame {
     public static void main(String[] args){
          WordNet turkishWordNet = new WordNet();
-         WordNet englishWordNet = new WordNet("Data/Wordnet/english_wordnet_version_31.xml", "Data/Wordnet/english_exception.xml", new Locale("en"));
+         WordNet englishWordNet = new WordNet("english_wordnet_version_31.xml", "english_exception.xml", new Locale("en"));
          new InterlingualRelationFrame(englishWordNet, turkishWordNet);
     }
 
